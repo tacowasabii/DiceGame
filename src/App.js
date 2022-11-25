7b4ae7d7e82cb92489fc8d28e0fa0685b4841a88
tacowasabii <godseoril812@gmail.com>
2022-11-26 02:26:12 +0900

    start dicegame

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Dice from "./Dice";
+
+function App() {
+  return (
+    <div>
+      <Dice color="blue" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dice.js
@@ -0,0 +1,9 @@
+import diceBlue01 from "./assets/dice-blue-1.svg";
+import diceRed01 from "./assets/dice-red-1.svg";
+
+function Dice(props) {
+  const diceImg = props.color === "red" ? diceRed01 : diceBlue01;
+  return <img src={diceImg} alt="주사위" />;
+}
+
+export default Dice;
