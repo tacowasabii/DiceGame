614dcfe3795d9bde556e014664935b4927328a64
tacowasabii <godseoril812@gmail.com>
2022-11-30 16:38:48 +0900

    add array state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import Button from "./Button";
 import Dice from "./Dice";
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+    setSum(0);
+    setGameHistory([]);
+  };
+
   return (
     <div>
-      <Button>던지기</Button>
-      <Button>처음부터</Button>
-      <Dice color="red" num={4} />
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <div>
+        <h2>나</h2>
+        <Dice color="blue" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        {gameHistory.join(", ")}
+      </div>
     </div>
   );
 }
